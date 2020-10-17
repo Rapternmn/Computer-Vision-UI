d414bc7c9ddb6c00d88123ae06c5451c9cd6af3d
Naman.Bhayani <namanbhayani.bitspilani@gmail.com>
2020-10-17 08:41:28 +0530

    Updated the API Endpoint

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // const base_url = "http://ec2-user@ec2-18-218-22-14.us-east-2.compute.amazonaws.com:8080"
-const base_url = "http://0.0.0.0:8080"
+const base_url = "http://ubuntu@ec2-18-224-73-223.us-east-2.compute.amazonaws.com:8080"
+// const base_url = "http://0.0.0.0:8080"
 
 export const predict = url => {
     return axios
